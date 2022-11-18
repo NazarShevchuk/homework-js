15ced7790308dfcbfd06d65446dd550350b145ac
NazarShevchuk <sevcuk031@gmail.com>
2022-11-18 21:15:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("finish");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+  const result = 0;
+
+  const a1 = document.getElementById("q1").value;
+
+  alert(a1);
+}
