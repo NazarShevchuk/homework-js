11b47f82a60236d8064ac769d5fa8bd4be0160e8
NazarShevchuk <sevcuk031@gmail.com>
2022-11-19 01:03:46 +0200

    done

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const button = document.getElementById("finish");
+let button = document.getElementById("finish");
 
 button.addEventListener("click", checkTest);
 
 function checkTest() {
-  const result = 0;
+  let result = 0;
 
-  const a1 = document.getElementById("q1").value;
+  let a1 = document.getElementById("q1").value;
 
-  alert(a1);
+  if (a1 === "6") {
+    result++;
+  }
+  let a2 = document.getElementById("q2").value;
+
+  if (a2 === "10") {
+    result++;
+  }
+  let a3 = document.getElementById("q3").value;
+
+  if (a3 === "12") {
+    result++;
+  }
+  let a4 = document.getElementById("q4").value;
+
+  if (a4 === "16") {
+    result++;
+  }
+  let a5 = document.getElementById("q5").value;
+
+  if (a5 === "27") {
+    result++;
+  }
+
+  alert("Количество правильных ответов: " + result);
 }
+
+checkTest();
